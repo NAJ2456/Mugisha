cb3a46649443d9616309a2588e02a913c114a5f3
NAJ2456 <mugishanajib5@gmail.com>
2024-10-23 13:06:49 +0300

    Update assignment.php

--- assignment.php
+++ assignment.php
@@ -1,18 +1,18 @@
 <?php
 
-function call_name(): string{
+function call_name(){
   $name = "KIWEWA NAJIBU MUGISHA";
 
 return $name;
 }
 
-function my_age($c_year, $dob): float|int{
+function my_age($c_year, $dob){
   $age = $c_year - $dob;
 return $age;
 }
 
-function home_address(): string{
-  $address = "Uganda-Kabale-Rwakaraba";
+function home_address(){
+  $address = "Uganda-kampala";
   return $address;
 }
 print (call_name()."! <br> I am a born of 2002 and my home address is ".home_address()."<br>"."I am ".my_age(c_year: 2024, dob: 2003));
